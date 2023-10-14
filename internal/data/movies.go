b2f3b0b38db1c1af000040b67631ac8662927f16
anukuljoshi <anukuljoshi1@gmail.com>
2023-10-15 02:06:44 +0530

    added routes, handlers for deleting a movie

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -100,5 +100,23 @@ func (m MovieModel) Update(movie *Movie) error {
 
 // delete a movie record with id from db
 func (m MovieModel) Delete(id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+	query := `
+		DELETE FROM movies
+		WHERE id = $1;
+	`
+	result, err := m.DB.Exec(query, id)
+	if err!=nil {
+		return err
+	}
+	rowsAffected, err := result.RowsAffected()
+	if err!=nil {
+		return err
+	}
+	if rowsAffected==0 {
+		return ErrRecordNotFound
+	}
 	return nil
 }
