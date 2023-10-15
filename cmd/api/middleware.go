a5033efd69e8555eb1428dbacf12aa3273b4f5f7
anukuljoshi <anukuljoshi1@gmail.com>
2023-10-15 21:49:33 +0530

    added a global rate limiter to limit access to apis

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"golang.org/x/time/rate"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -14,6 +16,21 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
 			}
 		}()
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) rateLimit(next http.Handler) http.Handler {
+	// initialize a rate limiter which allows an average of 2 req/sec
+	// with maximum of 4 req in a single burst
+	limiter := rate.NewLimiter(2, 4)
+	// returning function is a closure which closes over the limiter
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// call limiter.Allow() to check if request is permitted
+		if !limiter.Allow() {
+			app.rateLimitExceeded(w, r)
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
