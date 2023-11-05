bcc96ead56c25960b3cef259627b4ea6fb8fe643
anukuljoshi <anukuljoshi1@gmail.com>
2023-11-05 22:07:28 +0530

    added authentication middleware

---
+++ cmd/api/context.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/anukuljoshi/greenlight/internal/data"
+)
+
+type ContextKey string
+
+const UserContextKey = ContextKey("user")
+
+func (app *application) contextSetUser(r *http.Request, user *data.User) *http.Request {
+	ctx := context.WithValue(r.Context(), UserContextKey, user)
+	return r.WithContext(ctx)
+}
+
+func (app *application) contextGetUser(r *http.Request) *data.User {
+	user, ok := r.Context().Value(UserContextKey).(*data.User)
+	if !ok {
+		panic("missing user value in request context")
+	}
+	return user
+}
--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"net/http"
+	"strings"
 	"sync"
 	"time"
 
+	"github.com/anukuljoshi/greenlight/internal/data"
+	"github.com/anukuljoshi/greenlight/internal/validator"
 	"golang.org/x/time/rate"
 )
 
@@ -83,6 +87,54 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 			// unlock mutex before call next handler
 			mu.Unlock()
 		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+// authentication middleware
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Add the "Vary: Authorization" header to the response. This indicates to any
+		// caches that the response may vary based on the value of the Authorization
+		// header in the request.
+		w.Header().Add("Vary", "Authorization")
+		// retrieve Authorization header from request
+		authorizationHeader := r.Header.Get("Authorization")
+		// set anonymous user if Auth header is empty
+		if authorizationHeader=="" {
+			r = app.contextSetUser(r, data.AnonymousUser)
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		// extract token from Authorization header
+		headerParts := strings.Split(authorizationHeader, " ")
+		// check if Auth header is in correct format
+		if len(headerParts)!=2 || headerParts[0]!="Bearer" {
+			app.invalidAuthenticationTokenResponse(w, r)
+			return
+		}
+		token := headerParts[1]
+		// check if token is valid
+		v := validator.New()
+		if data.ValidateTokenPlainText(v, token); !v.Valid() {
+			app.invalidAuthenticationTokenResponse(w, r)
+			return
+		}
+		// get user with token
+		user, err := app.models.Users.GetForToken(data.ScopeAuthentication, token)
+		if err!=nil {
+			switch {
+			case errors.Is(err, data.ErrRecordNotFound):
+				app.invalidAuthenticationTokenResponse(w, r)
+			default:
+				app.serverErrorResponse(w, r, err)
+			}
+			return
+		}
+		// set user in request context
+		r = app.contextSetUser(r, user)
+		// call next handler in chain
 		next.ServeHTTP(w, r)
 	})
 }
